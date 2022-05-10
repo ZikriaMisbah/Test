e1713fe980e22149dc7ea61724941f5c317cb695
Zikria Misbah <zikriya.af@gmail.com>
2022-05-09 21:40:22 -0400

    changes

--- src/GitHubClass.java
+++ src/GitHubClass.java
@@ -2,7 +2,10 @@
 public class GitHubClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 50;
+		int c = a+b;
+		System.out.println(c);
 
 	}
 
